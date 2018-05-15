491721decdf186b537aafd94f274027ca1f85451
Rudinei Goi Roecker <rudineigr@itflex.com.br>
2018-05-15 11:10:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple AMQP PubSub
+
+A simple AMQP Pub/Sub lib for Python
+
+## Installation
+
+```bash
+# Asyncio
+pip install simple-amqp[asyncio] simple-amqp-pubsub
+
+# Gevent
+pip install simple-amqp[gevent] simple-amqp-pubsub
+```
+
+## Example usage
+
+see [examples](./examples)
---
+++ setup.py
@@ -0,0 +1,41 @@
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+basedir = path.abspath(path.dirname(__file__))
+
+with open(path.join(basedir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='simple-amqp-pubsub',
+    version='0.0.1',
+    description='Simple AMQP Pub/Sub lib',
+    long_description=long_description,
+    url='https://github.com/rudineirk/py-simple-amqp-pubsub',
+    author='Rudinei Goi Roecker',
+    author_email='rudinei.roecker@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='simple amqp pubsub',
+    packages=['simple_amqp_rpc'],
+    install_requires=[
+        'simple_amqp',
+    ],
+    extras_require={
+        'asyncio': [
+            'aio-pika',
+        ],
+        'gevent': [
+            'gevent',
+        ]
+    },
+)
