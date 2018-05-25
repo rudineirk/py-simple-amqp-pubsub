ed504918e9e1f1a504ca10dbee2681803ac77e51
Rudinei Goi Roecker <rudineigr@itflex.com.br>
2018-05-25 09:33:32 -0300

    release: 0.1.0

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ with open(path.join(basedir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='simple-amqp-pubsub',
-    version='0.0.5',
+    version='0.1.0',
     description='Simple AMQP Pub/Sub lib',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/rudineirk/py-simple-amqp-pubsub',
     author='Rudinei Goi Roecker',
     author_email='rudinei.roecker@gmail.com',
